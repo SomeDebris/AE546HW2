106f9f9aa969c2d28378895c7a5bb717d5478a1a
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-09-28 19:12:49 -0700

    Add most of starter code for Problem 1

--- src/AE546HW2.jl
+++ src/AE546HW2.jl
@@ -1,5 +1,43 @@
 module AE546HW2
 
+using Plots, ModelingToolkit, DifferentialEquations
+
 greet() = print("Hello World!")
+
+function p1b()
+    V_M = 300
+    V_T = 200
+    R   = 6000
+    β   = deg2rad(30)
+    θ   = deg2rad(20)
+    θ_T = 0
+    δt  = 0.02
+    T   = 20
+    N   = round(Int, δt / T)
+
+    # Allocate memory. Do not assign value (increases speed, but cannot assume that each value is a 0)
+    Rlog = Matrix{Float64}(undef, 1, N)
+    βlog = Matrix{Float64}(undef, 1, N)
+
+    R_dot = Float64(undef)
+    β_dot = Float64(undef)
+
+    # * KRIS!!!
+    # * we have to kill the fucking titan
+    # Shoot missile           ♡            Do nothing
+
+    for k in 1:N
+        R_dot = V_T * cos(β - θ_T) - V_M * cos(β - θ)
+        β_dot = -(V_T * sin(β - θ_T) - V_M * sin(β - θ)) / R
+
+        R = R + R_dot * δt
+        β = β + β_dot * δt
+
+        Rlog[k] = R
+        βlog[k] = β
+    end
+
+    t = (0:(N-1)) .* δt
+end
 
 end # module AE546HW2
